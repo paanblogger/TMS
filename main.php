de2c12b84f1976e7e211c7565d22435e790b1f3b
asipamn <kido_ec@yahoo.com>
2014-04-09 15:47:42 +0800

    CI
    TMS

---
+++ main.php
@@ -0,0 +1,41 @@
+<?php if (!defined('BASEPATH')) die();
+class Main extends Main_Controller {
+
+   public function index()
+	{
+      $this->load->view('include/header');
+      $this->load->view('login');
+      $this->load->view('include/footer');
+	}
+	public function login()
+	{		
+		$user = $this->input->post('log-username');
+		$pass = $this->input->post('log-password');
+		
+		if(!(($user=NULL) && ($pass = NULL)))
+		{
+			echo $user;
+			echo $pass;
+			$query = $this->db->query("SELECT * FROM user WHERE u_username = ".$user." AND u_pass = ".$pass.";");
+			$row = $query->row();
+			if ($query->num_rows() > 0)
+			{
+				$this->load->view('include/header');
+				$this->load->view('user/index');
+				$this->load->view('include/footer');
+			}
+		}
+		else
+			{
+				$this->load->view('include/header');
+				$this->load->view('login');
+				$this->load->view('include/footer');
+			}
+		
+		
+	}
+   
+}
+
+/* End of file frontpage.php */
+/* Location: ./application/controllers/frontpage.php */
